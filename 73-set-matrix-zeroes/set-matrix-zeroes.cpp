f9e35e1a020083cb95dd36eead0bdaf7a37d2764
tanishachauhan922 <tanishachauhan922@gmail.com>
2026-08-09 00:36:13 +0530

    Time: 0 ms (100.00%) | Memory: 20.9 MB (55.81%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -3,25 +3,32 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m=matrix.size();
         int n=matrix[0].size();
-        vector<pair<int,int>>st;
+       int col0=1;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    st.push_back({i,j});
+                   matrix[i][0]=0;
+                   if(j!=0) matrix[0][j]=0;
+                   else col0=0;
                 }
             }
         }
-        //to make
-        int size=st.size();
-        for(auto it : st){
-            int row=it.first;
-            int col=it.second;
-           for(int j=0;j<n;j++){
-            matrix[row][j]=0;
-           }
-              for(int j=0;j<m;j++){
-            matrix[j][col]=0;
-           }
+        //mark other than first row and first col
+        for(int i=1;i<m;i++){
+        for(int j=1;j<n;j++){
+            if(matrix[0][j]==0 || matrix[i][0]==0){
+              matrix[i][j]=0;   
+            }
         }
+        }
+        //now arrange first ones
+        if(matrix[0][0]==0){
+        for(int j=0;j<n;j++ ){
+             matrix[0][j]=0;
+    }
+        }
+     if(col0==0){
+        for(int i=0;i<m;i++)matrix[i][0]=0;
+     }
     }
 };
