98e19a5eb6d30b8382dde5160c35a3ecd81a087b
tanishachauhan922 <tanishachauhan922@gmail.com>
2026-08-12 00:22:19 +0530

    Time: 39 ms (72.20%) | Memory: 45.4 MB (55.75%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -2,22 +2,18 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int n=nums.size();
-        unordered_map<int,int>mp;
-        int sum=0;
+        unordered_map<int,int>mp; //konsa sum kitni bar aaya h
         int ans=0;
+        mp[0]=1;
+        int sum=0;
         for(int i=0;i<n;i++){
-            sum+=nums[i];
-             if(sum==k){
-                ans++;
-             }
-             int rem=sum-k;
-             if(mp.find(rem)!=mp.end()){
-                ans+=mp[rem];
-             }
-
-                mp[sum]++;
-             }
-
+              sum+=nums[i];
+              if(mp.find(sum-k)!=mp.end()){
+                ans+=mp[sum-k];
+              }
+              mp[sum]++;
+        }
         return ans;
+        
     }
 };
