e50b47d4e25102462db2261811b93d8399b36b2f
tanishachauhan922 <tanishachauhan922@gmail.com>
2026-08-16 11:04:57 +0530

    Time: 79 ms (12.02%) | Memory: 94.2 MB (16.65%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -3,18 +3,19 @@ public:
     int numberOfSubarrays(vector<int>& nums, int k) {
         int n=nums.size();
         int ans=0;
-        unordered_map<int,int>mp;//to store odd freq
+        unordered_map<int,int>mp;
         mp[0]=1;
         int cnt=0;
+        
         for(int i=0;i<n;i++){
             if(nums[i]%2!=0){
-                cnt++;
+                 cnt++;
             }
+            mp[cnt]++;
             if(mp.find(cnt-k)!=mp.end()){
                 ans+=mp[cnt-k];
             }
-            mp[cnt]++;
-    }
+        }
         return ans;
     }
 };
