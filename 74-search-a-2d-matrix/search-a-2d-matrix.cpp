df1f09815685aea8d81ef988b317e3951e87a9fa
tanishachauhan922 <tanishachauhan922@gmail.com>
2026-08-26 00:50:00 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (6.83%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+
+        int s=0;
+        int e=(m*n)-1;
+        while(s<=e){
+            int mid=(s+e)/2;
+            int r=mid/n;
+            int c=mid%n;
+            if(matrix[r][c]==target) return true;
+            else if(matrix[r][c]<target) s=mid+1;
+            else e=mid-1;
+        }
+        return false;
+    }
+};
